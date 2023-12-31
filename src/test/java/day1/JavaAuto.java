8ac7e45fc57c2884bba0921c1dd7ded344a3a43a
Mayuri <bhutekarmayuri10@gmail.com>
2023-12-31 16:50:36 +0530

    .

--- src/test/java/day1/JavaAuto.java
+++ src/test/java/day1/JavaAuto.java
@@ -5,5 +5,6 @@ public class JavaAuto {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hi");
+        System.out.println("Mayuri");
     }
 }
