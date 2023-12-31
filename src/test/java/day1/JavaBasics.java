1f756fe107bb626acc5dfcb9af78dc35e0bd4803
unknown <bhutekarmayuri10@gmail.com>
2023-12-31 16:43:38 +0530

    first commit

---
+++ src/test/java/day1/JavaBasics.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class JavaBasics {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hi");
+    }
+
+
+
+}
